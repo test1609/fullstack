dab13745edd5aa58df055e7e1068089f61e07d19
test1609 <shraddha.bhuva16@gmail.com>
2023-01-13 11:49:58 -0500

    added component changes

--- lecture1/public/App.jsx
+++ lecture1/public/App.jsx
@@ -1,6 +1,40 @@
-const element = (
-    <div id="root-2">
-        <h2>Hello from index file</h2>
-    </div>
-)
-ReactDOM.render(element, document.getElementById("root-1")); 
+// const element = (
+//      (<div id="root-2">
+//             <h2>Hello from index file</h2>
+//         </div>)
+// )
+// ReactDOM.render(element, document.getElementById("root-1")); 
+
+//class component
+class Greeting extends React.Component {
+    render() {
+
+        return (<div id="root-2">
+            <h2>Hello from index file</h2>
+        </div>)
+
+    }
+}
+
+//functional component
+function Welcome(subject){
+    return(
+        <div id="root-3"> 
+            <h3>Welcome to {subject.subject}</h3>
+        </div>
+    )
+}
+
+const subject = "Adv full-stack programming";
+
+const App = ()=>{
+    return(
+        <div>
+            <Greeting></Greeting>
+            <Welcome subject={subject}></Welcome>
+        </div>
+    )
+}
+
+const element = ReactDOM.createRoot(document.getElementById("root-1"));
+element.render(<App></App>)
